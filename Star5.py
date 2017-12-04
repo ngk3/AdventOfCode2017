14448cfe40df55c21c783193e489ff9b82b22636
ngk3 <kevin.ng.ngk3@gmail.com>
2017-12-04 04:32:11 +0000

    Added comments to Star5 and Star6

--- Star5.py
+++ Star5.py
@@ -1,13 +1,17 @@
+# Looking at this problem with the idea that the (0, 0) starts in the top left corner
 
+# Function used to get the distance from the coordinate to the middle initial starting point 
 def get_distance(coordinates, upper_bound):
     mid_coordinate = [upper_bound / 2, upper_bound / 2]
     print abs(coordinates[0] - (upper_bound / 2)) + abs(coordinates[1] - (upper_bound / 2))
 
+# Function used to find the end coordinates of the input 
 def find_coordinate(input, lower_bound, upper_bound):
+    # This means that the input is in the bottom right corner 
     if lower_bound == upper_bound:
-        print [upper_bound - 1, upper_bound - 1]
         return [upper_bound - 1, upper_bound - 1]
         
+    # Move the coordinates backwards until the right corner is found
     coordinate = [upper_bound - 1, upper_bound - 1]
     highest_num = upper_bound**2
     if highest_num - (upper_bound - 1) > input:
@@ -19,25 +23,21 @@ def find_coordinate(input, lower_bound, upper_bound):
     if highest_num - (upper_bound - 1) > input:
         coordinate = [upper_bound - 1, 0]
         highest_num -= (upper_bound - 1)
-        
-    print coordinate, highest_num - input 
-        
+    
+    # Move the coordinates backwards until the right number is found and return the coordinates
     if coordinate == [upper_bound - 1, upper_bound - 1]:
         coordinate[0] = highest_num - input
-        print "Got here 1"
     elif coordinate == [0, upper_bound - 1]:
         coordinate[1] = highest_num - input
-        print "Got here 2"
     elif coordinate == [0, 0]:
         coordinate[0] = highest_num - input
-        print "Got here 3"
     else: 
         coordinate[1] = highest_num - input
-        print "Got here 4"
-
-    print coordinate
     return coordinate
     
+# Function that finds the upper and lower boundary of the input
+# This takes advantage of the pattern that each wraparound spiral is in the pattern 1, 9, 25, ...
+# OR (1^2), (3^2), (5^2), ...
 def get_boundary(input):
     lower_bound = int(input**(1/2.0))
     if lower_bound % 2 == 0:
@@ -45,26 +45,8 @@ def get_boundary(input):
     upper_bound = lower_bound 
     if lower_bound ** 2 != input:
         upper_bound += 2
-    print lower_bound 
-    print upper_bound 
+    return lower_bound, upper_bound 
 
 # Open the file and call the function
-print 
-get_boundary(1)
-get_distance(find_coordinate(1, 1, 1), 1)
-
-print 
-get_boundary(12)
-get_distance(find_coordinate(12, 3, 5), 5)
-
-print
-get_boundary(23)
-get_distance(find_coordinate(23, 3, 5), 5)
-
-print 
-get_boundary(1024)
-get_distance(find_coordinate(1024, 31, 33), 33)
-
-print 
-get_boundary(289326)
-get_distance(find_coordinate(289326, 537, 539), 539)
+bounds = get_boundary(289326)
+get_distance(find_coordinate(289326, bounds[0], bounds[1]), bounds[1])   
--- Star6.py
+++ Star6.py
@@ -1,11 +1,15 @@
 
+# Function used to search for the highest value given the conditions of the spiral greater than input_value
 def get_values(input_value):
+    # Used to track the situation and the coordinate values 
     values = {}
     values["0 0"] = 1
     position = [0, 0]
     tracker = 0
     tracker_2 = -1
+    # Loop this until a value is found satisfying the before mentioned conditions
     while True:
+        # If at the end of the spiral, move right one space, check if value fits and then continue 
         if position == [tracker, tracker]:
             position = [tracker + 1, tracker]
             sum_around = get_adjacent_sum(values, position)
@@ -15,6 +19,7 @@ def get_values(input_value):
             tracker += 1
             tracker_2 += 2
         else:
+            # Going up the spiral 
             for i in range(tracker_2):    
                 position = [position[0], position[1] - 1]
                 sum_around = get_adjacent_sum(values, position)
@@ -22,13 +27,15 @@ def get_values(input_value):
                     return sum_around
                 values[str(position[0]) + " " + str(position[1])] = get_adjacent_sum(values, position)
             
+            # Going left of spiral
             for j in range(tracker_2 + 1):
                 position = [position[0] - 1, position[1]]
                 sum_around = get_adjacent_sum(values, position)
                 if sum_around > input_value:
                     return sum_around
                 values[str(position[0]) + " " + str(position[1])] = get_adjacent_sum(values, position)
-                
+            
+            # Goind down of spiral 
             for j in range(tracker_2 + 1):
                 position = [position[0], position[1] + 1]
                 sum_around = get_adjacent_sum(values, position)
@@ -36,17 +43,21 @@ def get_values(input_value):
                     return sum_around
                 values[str(position[0]) + " " + str(position[1])] = get_adjacent_sum(values, position)
             
+            # Completing the spiral loop
             for j in range(tracker_2 + 1):
                 position = [position[0] + 1, position[1]]
                 sum_around = get_adjacent_sum(values, position)
                 if sum_around > input_value:
                     return sum_around
                 values[str(position[0]) + " " + str(position[1])] = get_adjacent_sum(values, position)
-        
+
+# Function used to add all the adjacent values up given the list of current values and the current position                
 def get_adjacent_sum(values, position):
+    # All the coordinates that surround position
     adj_values = [[position[0] + 1, position[1]], [position[0] + 1, position[1] - 1], [position[0] + 1, position[1] + 1], 
                   [position[0] - 1, position[1]], [position[0] - 1, position[1] + 1], [position[0] - 1, position[1] - 1], 
                   [position[0], position[1] + 1], [position[0], position[1] - 1]]
+    # Add the coordinates together if they exist in values
     total = 0
     for av in adj_values:
         coord = str(av[0]) + " " + str(av[1])
