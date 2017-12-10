90050ce3eb81a1551fbb0fab1f41bff84af3ca05
ngk3 <kevin.ng.ngk3@gmail.com>
2017-12-10 20:43:36 +0000

    Added comments to Star13.py. This program has borh Star13 and Star 14

--- Star13.py
+++ Star13.py
@@ -1,14 +1,20 @@
 
+# Class used to represent a Tree
 class Tree:
+    # all_nodes will store all the nodes, root is the bottom program, 
+    # weights stores the total weights of each node 
     all_nodes = []
     root = None
     weights = {}
     
+    # Function used to create a node and insert into all_nodes
     def create_node_no_children(self, info):
         info_splitted = info.split(" ")
         self.all_nodes.append(Node(info_splitted[0], int(info_splitted[1])))
 
+    # Function used to connect parent-children relationships of nodes 
     def create_node_parents(self, info, children):
+        # Get the indices of the parent and children nodes in all_nodes
         node_index = -1
         children_index = []
         node_name = info.split(" ")[0]
@@ -18,11 +24,14 @@ class Tree:
                 node_index = an 
             elif self.all_nodes[an].name in children_name:
                 children_index.append(an)
+        # Set the children and parent accordingly
         for ci in children_index:
             self.all_nodes[node_index].children.append(self.all_nodes[ci])
             self.all_nodes[ci].parent = self.all_nodes[node_index]
     
+    # Function that reads an input file and create the nodes and the node relationships
     def read_file_and_get_nodes(self, file_name):
+        # Read the file and create the nodes 
         node_relationships = {}
         for line in open(file_name):
             splitted = line.strip().split(" -> ")
@@ -31,9 +40,11 @@ class Tree:
             if len(splitted) == 2:
                 node_relationships[splitted[0]] = splitted[1]
         
+        # Set the relationships of the nodes
         for node_info in node_relationships.keys():
             self.create_node_parents(node_info, node_relationships[node_info])
-        
+    
+    # Function used to get and set the root node 
     def get_parent(self):
         index = 0
         self.root = self.all_nodes[0]
@@ -41,6 +52,7 @@ class Tree:
             self.root = self.root.parent
         return self.root 
         
+    # Function used to print the tree information 
     def print_node_info(self):
         for n in self.all_nodes:
             s = n.name + " " + str(n.weight)
@@ -52,7 +64,9 @@ class Tree:
                 s += "(Parent = " + n.parent.name + ")"
             print s.strip()
 
+    # Function used to calculate and set the combined weights of each node 
     def get_weights(self, start):
+        # Recursively find the combined weights of each node starting from the root and going up
         if len(start.children) == 0:
             self.weights[start.name] = start.weight
             return start.weight 
@@ -65,7 +79,9 @@ class Tree:
         self.weights[start.name] = w
         return w 
     
+    # Function used to find the one incorrect weight and return the change needed to balance the programs 
     def find_incorrect_weight_and_return_balanced_weight(self):
+        # Find the parent of the incorrect weight 
         visited = []
         visited.append(self.root)
         current_node = None 
@@ -78,9 +94,25 @@ class Tree:
                 visited.append(c)
             if len(c_weights) == 2:
                 found_node = current_node
+        # Get the new weight needed to balance the tower
+        frequency = {}
         for c in found_node.children:
-            print c.name, c.weight,": " + str(self.weights[c.name])
-            
+            if self.weights[c.name] in frequency.keys():
+                frequency[self.weights[c.name]] += 1
+            else:
+                frequency[self.weights[c.name]] = 1
+        different_value = 0
+        same_values = 0
+        for f in frequency.keys():
+            if frequency[f] == 1:
+                different_value = f
+            else:
+                same_values = f
+        for c in found_node.children:
+            if self.weights[c.name] == different_value:
+                return c.weight + (same_values - different_value)
+
+# The Node class used to store information of each program            
 class Node:
     name = ""
     weight = -1
@@ -96,10 +128,8 @@ class Node:
 
 t = Tree()
 t.read_file_and_get_nodes("star13_input.txt")
-#t.print_node_info()
 
 n = t.get_parent()
-print n.name
+print "PART 1: ", n.name
 t.get_weights(n)
-print t.weights
-t.find_incorrect_weight_and_return_balanced_weight()
+print "PART 2: ",   t.find_incorrect_weight_and_return_balanced_weight()
